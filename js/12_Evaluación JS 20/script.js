09f26c282d6a7403b6b9d07d4e1188d3ed1e86ac
arielgonzalezvvh <165287986+arielgonzalezvvh@users.noreply.github.com>
2025-04-24 11:14:04 -0400

    ok

--- js/12_Evaluación JS 20/script.js
+++ js/12_Evaluación JS 20/script.js
@@ -1,20 +1,23 @@
-// Creamos un array para guardar los números ingresados
-
 function filtrarPares() {
     let numeros = [];
     let cantidad = parseInt(prompt("¿Cuántos números quieres ingresar?"));
     let pares = [];
+
     
     for (let i = 0; i < cantidad; i++) {
         let valor = parseInt(prompt("Ingresa un número:"));
         numeros.push(valor);
     }
-    
+
     for (let i = 0; i < numeros.length; i++) {
         if (numeros[i] % 2 === 0) {
-        pares.push(numeros[i]);
+            pares.push(numeros[i]);
         }
     }
-    
-    alert("Números pares: " + pares.join(", "));
+
+    if (pares.length === 0) {
+        alert("No hay números pares");
+    } else {
+        alert("Números pares: " + pares.join(", "));
     }
+}
