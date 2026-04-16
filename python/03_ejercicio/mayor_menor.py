a65da7f49309372b84264f19207c061411a784d4
arielgonzalezvvh <arielgonzalez@liceovvh.cl>
2026-04-15 21:13:25 -0400

    ok

---
+++ python/03_ejercicio/mayor_menor.py
@@ -0,0 +1,28 @@
+edad = 18
+
+if edad >= 18:
+    print ("Es mayor de edad")
+else:
+    print("Es menor de edad")
+
+  #!= es  distinto a que   
+
+
+    # opcion con and y esto las dos concines tienq ue ser verdadera
+    edad = 18
+    tiene_licencia = True
+
+    if edad >= 18 and tiene_licencia:
+         print ("puede conducir")
+    else:
+        print("no puede conducir")
+
+        
+    # opcion con or solo falta que una sea veradera para que alga el verdadero
+    edad = 18
+    tiene_licencia = True
+
+    if edad >= 18 and tiene_licencia:
+        print ("puede conducir")
+    else:
+        print("no puede conducir")
---
+++ python/04_ejercicio/if_else_elif.py
@@ -0,0 +1,12 @@
+
+# elif es como un preguntas intermedias
+edad = 50
+
+if edad < 18:
+    print("eres menor de edad")
+elif 18 <= edad <=40:
+    print("ere adulto jovem")
+elif 41 <= edad < 65:
+    print("eres adulto")
+else:
+    print("eres un adulto mayor")
