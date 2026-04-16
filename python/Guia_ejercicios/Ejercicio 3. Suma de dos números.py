697b9de25f0c89de6b9405ace90e1bd9e0b34ee3
arielgonzalezvvh <arielgonzalez@liceovvh.cl>
2026-04-15 21:25:32 -0400

    ok

---
+++ python/Guia_ejercicios/Ejercicio 3. Suma de dos números.py
@@ -0,0 +1,7 @@
+num1 = int(input("dame un número = "))
+num2 = int(input("dame otro número = "))
+
+
+resultado = num1 + num2
+
+print("resultado de la suma = ", resultado)
---
+++ python/Guia_ejercicios/Ejercicio 4. Par o impar.py
@@ -0,0 +1,7 @@
+num1 = int(input("Dame un número para saber si es par o impar ="))
+
+if num1 % 2 == 0:
+        print("El número es par")
+else:
+        print("El número es impar")
+
