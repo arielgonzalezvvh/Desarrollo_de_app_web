a1cf0cb06d81b717199459c43fc2d1eca1365c6a
arielgonzalezvvh <arielgonzalez@liceovvh.cl>
2026-04-15 21:13:30 -0400

    ok

---
+++ python/Guia_ejercicios/Ejercicio 1. Hola estudiante.py
@@ -0,0 +1,4 @@
+nombre = input(" Me dices tu nombre  estudiante ")
+
+
+print("Hola eres el mejor", nombre)
---
+++ python/Guia_ejercicios/Ejercicio 2. Edad futura.py
@@ -0,0 +1,5 @@
+edad = int(input("Ingrese tu edad = "))  
+
+resultado = edad + 5
+
+print("La edad dentro de 5 años = ", resultado) 
